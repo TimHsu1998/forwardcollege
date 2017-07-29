fcd2bb4ee24be3283660307fb2e4a00ad018e58a
Tim <s6215s5216@gmail.com>
2017-07-29 09:04:19 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-categories = Category.create([{ name: '讀書' }, { name: '演講' }])
+categories = Category.create([{ name: '站在巨人的肩膀上' }, { name: '傳承手中的火把' }, { name: '留下走過的足跡' }
+  , { name: '找回最親近的相處' }, { name: '有任務的旅行' }, { name: 'Face to Face' }, { name: '再見心中的憾' }
+  , { name: '成為聯繫緣分的橋梁' }, { name: '用生命點燃希望' }, { name: '將影響從點變成面' }, { name: '寫下你的墓誌銘' }
+  , { name: '尋找根本' }, { name: '大學十全' }])
